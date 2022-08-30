186a18eb38e51950568314080ec7cce87bb8e3ac
charlescyso <charlescyso@gmail.com>
2022-08-29 18:49:13 -0700

    Reformat function order and grouping which lead to success of cells properly updating with player marks alternating

--- script.js
+++ script.js
@@ -21,46 +21,56 @@ const displayController = (() => {
 
     // events
     cells.forEach(cell => cell.addEventListener('click', (event) => {
-        gameController.updateBoard(event.target.dataset.index);
+        gameController.playRound(event.target.dataset.index);
         render();
     }));
 })();
 
-// game module
-const gameController = (() => {
-
-    // updates gameBoard.board array item with player's mark
-    const updateBoard = (index) => {
-        gameBoard.board[index] = player1.getMark();
-    };
-
-
-
-    const checkWin = () => {
-        const winConditions = [
-            [0, 1, 2], // rows
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6], // columns
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8], // diagonals
-            [2, 4, 6],
-        ];
-    }
-
-    return {updateBoard};
-})();
-
-
 // player factory
 const Player = (mark) => {
-    const getMark = () => mark;
-    
+    const getMark = () => {
+        return mark;
+    }
+
     return {getMark};
 };
 
+// game module
+const gameController = (() => {
+    const player1 = Player('X');
+    const player2 = Player('O');
+    let round = 1;
 
-// players
-const player1 = Player('X');
-const player2 = Player('O');
+    // play a single round
+    const playRound = (index) => {
+        if (gameBoard.board[index] !== '') {
+            return;
+        } else {
+        updateBoard(index);
+        round++;
+    }};
+
+    // updates gameBoard.board array item with player's mark
+    const updateBoard = (index) => {
+        gameBoard.board[index] = getCurrentPlayer().getMark();
+    };
+
+    const getCurrentPlayer = () => {
+        return round % 2 === 0 ? player2 : player1;
+    }
+
+    // const checkWin = () => {
+    //     const winConditions = [
+    //         [0, 1, 2], // rows
+    //         [3, 4, 5],
+    //         [6, 7, 8],
+    //         [0, 3, 6], // columns
+    //         [1, 4, 7],
+    //         [2, 5, 8],
+    //         [0, 4, 8], // diagonals
+    //         [2, 4, 6],
+    //     ];
+    // }
+
+    return {playRound};
+})();
